fbdd2993861ee26934e563601313de887684ce9f
Khushi Panchal <khushimpanchal03@gmail.com>
2025-02-05 17:55:06 +0530

    server setup

---
+++ backend/src/db/connect.js
@@ -0,0 +1,15 @@
+// import { config } from "dotenv";
+import mongoose from "mongoose";
+
+const connect = async () => {
+    try {
+        console.log("Attempting to connect to database...");
+        await mongoose.connect(process.env.MONGO_URI, {})
+        console.log("Connected to database...");
+    } catch (error) {
+        console.log("Failed to connect to database...")
+        process.exit(1);
+    }
+}
+
+export default connect;
